0f3eb6320dbb457e90a60ba0778d34b9ee087898
LorenCuadero <21103903@usc.edu.ph>
2022-10-18 10:55:34 +0800

    UPDATE

--- jquery-sample/ajax-jquery.js
+++ jquery-sample/ajax-jquery.js
@@ -10,5 +10,27 @@ $(function(){
       let value = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
       $("#showDate").text(value);
     });	
+
     
 })
+$.ajax({
+  url: 'https://api.weatherstack.com/current',
+  data: {
+    access_key: 'YOUR_ACCESS_KEY',
+    query: 'New York'
+  },
+  dataType: 'json',
+  success: function(apiResponse) {
+    console.log(`Current temperature in ${apiResponse.location.name} is ${apiResponse.current.temperature}℃`);
+  }
+  
+});
+var dt = new Date();
+    var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+    document.write(time);
+    
+    $.get(url, (data) => {
+      $("#startdate").text(data.title);}
+    )
+
+
